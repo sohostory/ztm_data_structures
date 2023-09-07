33128bc6cfaa486c01d85f49d33b718595ce90de
Hoyoung Lee <sohostory@gmail.com>
2023-09-07 14:03:58 -0500

    big O

--- Big O/goodCode.js
+++ Big O/goodCode.js
@@ -1,11 +1,31 @@
+const fish = ["dory", "bruce", "marlin", "nemo"];
 const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+const large = new Array(10).fill("nemo");
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("Found Nemo!");
     }
   }
+  let t1 = performance.now();
+
+  console.log("Call to find Nemo took " + (t1 - t0) + "ms");
 }
 
 findNemo(nemo);
+findNemo(everyone);
+findNemo(large);
