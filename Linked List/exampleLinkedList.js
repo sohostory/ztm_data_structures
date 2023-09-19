82befd482b392f1bb01c1fd39089108221879295
Hoyoung Lee <sohostory@gmail.com>
2023-09-19 14:48:28 -0500

    linked list

---
+++ Linked List/exampleLinkedList.js
@@ -0,0 +1,71 @@
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+    return this;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    console.log("current head: ", this.head);
+    console.log("current tail: ", this.tail);
+    this.tail.next = newNode;
+    console.log("updated head: ", this.head);
+    console.log("updated tail", this.tail);
+    this.tail = newNode;
+    console.log("new head: ", this.head);
+    console.log("new tail", this.tail);
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+// console.log("created head:");
+// console.log(myLinkedList);
+myLinkedList.append(5);
+// console.log("first append:");
+// console.log(myLinkedList);
+myLinkedList.append(6);
+// console.log("second append:");
+// console.log(myLinkedList);
+// console.log("next: ", myLinkedList.head.next);
+myLinkedList.append(10);
+// console.log("third append:");
+// console.log(myLinkedList);
+// console.log("next: ", myLinkedList.head.next);
+myLinkedList.prepend(7);
+// console.log("first prepend:");
+// console.log(myLinkedList);
