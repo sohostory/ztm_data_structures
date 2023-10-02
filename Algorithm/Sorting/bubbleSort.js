e1281c1f008f8799bb8b5be79547769814f526de
sohostory <sohostory@gmail.com>
2023-10-02 09:22:20 -0500

    selection sort

--- Algorithm/Sorting/bubbleSort.js
+++ Algorithm/Sorting/bubbleSort.js
@@ -3,11 +3,11 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 function bubbleSort(array) {
   const length = array.length;
   for (let i = 0; i < length; i++) {
-    for (let k = 0; k < length; j++) {
+    for (let j = 0; j < length - 1; j++) {
       if (array[j] > array[j + 1]) {
         // Swap numbers
         let temp = array[j];
-        array[j] = arrray[j + 1];
+        array[j] = array[j + 1];
         array[j + 1] = temp;
       }
     }
