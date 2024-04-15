ab65ff2f5fa92bb9224be1a5b58395c615fd9208
Hoyoung Lee <sohostory@gmail.com>
2024-04-15 16:38:33 -0500

    TS minor change

--- Data Structure/Arrays/reverseStringTs.js
+++ Data Structure/Arrays/reverseStringTs.js
@@ -1,9 +1,13 @@
 function reverseStr(str) {
+    if (!str || str.length < 2) {
+        return "not a valid input";
+    }
     var strArray = str.split("");
     var newArray = [];
-    for (var i = strArray.length - 1; i >= 0; i--) {
+    for (var i = strArray.length - 1; (i = 0); i--) {
         newArray.push(strArray[i]);
     }
     return newArray.join("");
 }
+console.log(reverseStr(""));
 console.log(reverseStr("hello"));
--- Data Structure/Arrays/reverseStringTs.ts
+++ Data Structure/Arrays/reverseStringTs.ts
@@ -1,12 +1,17 @@
 function reverseStr(str: string): string {
+  if (!str || str.length < 2) {
+    return "not a valid input";
+  }
+
   const strArray = str.split("");
   const newArray: string[] = [];
 
-  for (let i = strArray.length - 1; i >= 0; i--) {
+  for (let i = strArray.length - 1; (i = 0); i--) {
     newArray.push(strArray[i]);
   }
 
   return newArray.join("");
 }
 
+console.log(reverseStr(""));
 console.log(reverseStr("hello"));
